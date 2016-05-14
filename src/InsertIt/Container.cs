b06cf02af953bb3fcff322d6ad9b7f511bf65302
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-05-15 01:01:57 +0200

    Add solution, add first try to register and resolve dependencies

---
+++ src/InsertIt/Container.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertIt
+{
+    public class Container
+    {
+        private static readonly Dictionary<Type, Type> RegistredItems = new Dictionary<Type, Type>(); 
+        public Container(Action<RegistredItem> action)
+        {
+            action.Invoke(new RegistredItem());
+        }
+
+        public class RegistredItem
+        {
+            public Type Register { get; set; }
+            public Type Qua { get; set; }
+
+            public RegistredItem Record<TItem>()
+            {
+                Register = typeof(TItem);
+                return this;
+            }
+
+            public RegistredItem As<TItem>()
+            {
+                Qua = typeof (TItem);
+                RegistredItems.Add(Register, Qua);
+                return this;
+            }
+        }
+
+        public TItem Resolve<TItem>()
+        {
+            var requestType = typeof (TItem);
+            var resolvedType = RegistredItems[requestType];
+            return (TItem)Activator.CreateInstance(resolvedType);
+        }
+    }
+}
---
+++ tests/InsertIt.Tests/Container_tests.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace InsertIt.Tests
+{
+    // ReSharper disable once InconsistentNaming
+    public class Container_tests
+    {
+        [Fact]
+        public void should_register_properly()
+        {
+            var container = new Container(x =>
+            {
+                x.Record<ITestFirst>().As<Test>();
+                x.Record<ITestSecond>().As<Test>();
+            });
+            var resolveFirst = container.Resolve<ITestFirst>();
+            var resolveSecond = container.Resolve<ITestSecond>();
+            resolveFirst.ShouldBeOfType<Test>();
+            resolveSecond.ShouldBeOfType<Test>();
+        }
+
+    }
+
+    internal interface ITestFirst
+    {
+    }
+
+    internal class Test : ITestFirst, ITestSecond
+    {
+    }
+
+    internal interface ITestSecond
+    {
+        
+    }
+}
